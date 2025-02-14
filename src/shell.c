b00f4fa197418a973a38af9918c92c8c60fb9273
ThearchyHelios <work@thearchyhelios.com>
2025-02-14 18:25:53 +0100

    commit pipe finished

--- src/shell.c
+++ src/shell.c
@@ -2,7 +2,7 @@
  * @Author: ThearchyHelios work@thearchyhelios.com
  * @Date: 2025-02-13 08:17:24
  * @LastEditors: ThearchyHelios work@thearchyhelios.com
- * @LastEditTime: 2025-02-13 11:15:48
+ * @LastEditTime: 2025-02-14 18:21:07
  * @FilePath: /APNEE1/src/shell.c
  * @Description:
  */
@@ -17,30 +17,6 @@
 #include <string.h>
 
 #define BUFSIZE 1024
-
-char *gerer_tube(char *buffin, char *buffout)
-{
-	int fd[2];
-	pipe(fd);
-	pid_t child_pid = Fork();
-	int bytesin, bytesout;
-	bytesin = strlen(buffin);
-	if (child_pid != 0)
-	{ /* pere */
-		Close(fd[0]);
-		bytesout = write(fd[1], buffout, strlen(buffout) + 1);
-		printf("[%d]: write %d bytes, send %s to my child\n",
-					 getpid(), bytesout, buffout);
-	}
-	else
-	{ /* fils */
-		Close(fd[1]);
-		bytesin = read(fd[0], buffin, BUFSIZE);
-		printf("[%d](fils): read %d bytes, my bufin is {%s} \n",
-					 getpid(), bytesin, buffin);
-	}
-	return buffout;
-}
 
 int main()
 {
@@ -71,6 +47,29 @@ int main()
 		if (l->out)
 			printf("out: %s\n", l->out);
 
+		// 确定命令的数量
+		int nb_cmd = 0;
+		while (l->seq[nb_cmd] != 0)
+		{
+			nb_cmd++;
+		}
+		if (nb_cmd == 0)
+		{
+			continue;
+		}
+
+		int pipefd[nb_cmd - 1][2]; // 用于存储管道的文件描述符
+		for (i = 0; i < nb_cmd - 1; i++)
+		{
+			if (pipe(pipefd[i]) == -1)
+			{
+				perror("pipe");
+				exit(1);
+			}
+		}
+
+		pid_t pids[nb_cmd];
+
 		/* Display each command of the pipe */
 		for (i = 0; l->seq[i] != 0; i++)
 		{
@@ -83,27 +82,23 @@ int main()
 				exit(0);
 			}
 
-			// gerer tube
-				if (l->seq[i + 1] != 0)
-				{
-					char buffin[BUFSIZE];
-					char buffout[BUFSIZE];
-					strcpy(buffin, l->seq[i]);
-					strcpy(buffout, l->seq[i+1]);
-					gerer_tube(buffin, buffout);
-				}
+			// pid_t pid;
+			pids[i] = Fork();
 
-			pid_t pid;
-			pid = Fork();
-
-			switch (pid)
+			switch (pids[i])
 			{
 			case -1:
 				unix_error("Fork error");
 				exit(1);
 				break;
 			case 0:
-				if (l->in) // redirection de l'entree standard
+				if (i > 0) // redirection de l'entree standard
+				{
+					int in_pipe = pipefd[i - 1][0];
+					Dup2(in_pipe, STDIN_FILENO);
+					Close(pipefd[i - 1][0]);
+				}
+				else if (l->in) // redirection de l'entree standard
 				{
 					int fd = open(l->in, O_RDONLY, 0644);
 					if (fd < 0)
@@ -114,8 +109,13 @@ int main()
 					Dup2(fd, STDIN_FILENO);
 					close(fd);
 				}
-
-				if (l->out) // redirection de la sortie standard
+				if (i < nb_cmd - 1) // redirection de la sortie standard
+				{
+					int out_pipe = pipefd[i][1];
+					Dup2(out_pipe, STDOUT_FILENO);
+					Close(pipefd[i][1]);
+				}
+				else if (l->out) // redirection de la sortie standard
 				{
 					int fd = open(l->out, O_WRONLY | O_CREAT, 0644);
 					if (fd < 0)
@@ -126,18 +126,30 @@ int main()
 					Dup2(fd, STDOUT_FILENO);
 					close(fd);
 				}
-				int code = execvp(cmd[0], cmd);
-				if (code == -1)
+				// 关闭所有管道
+				for (j = 0; j < nb_cmd - 1; j++)
 				{
-					perror("execvp error ");
-					exit(-1);
+					close(pipefd[j][0]);
+					close(pipefd[j][1]);
 				}
-				exit(0);
+
+				if (execvp(cmd[0], cmd) == -1)
+				{
+					perror("execvp error");
+					exit(1);
+				}
 				break;
 			default:
-				waitpid(pid, NULL, 0);
-				if (pid < 0)
-					unix_error("waitpid error");
+				// 如果不是第一个命令，关闭上一个管道的读端
+				if (i > 0)
+				{
+					close(pipefd[i - 1][0]);
+				}
+				// 如果不是最后一个命令，关闭当前管道的写端
+				if (i < nb_cmd - 1)
+				{
+					close(pipefd[i][1]);
+				}
 				break;
 			}
 			for (j = 0; cmd[j] != 0; j++)
@@ -145,6 +157,15 @@ int main()
 				printf("%s ", cmd[j]);
 			}
 			printf("\n");
+		}
+		for (i = 0; i < nb_cmd; i++)
+		{
+			int status;
+			waitpid(pids[i], &status, 0);
+			if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
+			{
+				printf("Command failed with status %d\n", WEXITSTATUS(status));
+			}
 		}
 	}
 }
